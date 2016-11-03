f443251e91b06c38d46d003acedeeffbcb8024f0
John W. Long <me@johnwlong.com>
2016-11-03 14:29:25 -0400

    Working tests

--- hello_spec.cr
+++ hello_spec.cr
@@ -1,9 +1,31 @@
-require "hello"
+require "spec"
+require "./hello"
 
-describe "Hello" do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+describe "hello" do
+  it "prints 'Hello world!'" do
+    result = run "./hello"
+    result[:output].should eq("Hello world!\n")
   end
+
+  it "prints 'Hello John!'" do
+    result = run "./hello John"
+    result[:output].should eq("Hello John!")
+  end
+
+  it "prints 'Hello John Jacob Jingleheimer Schmidt!'" do
+    result = run "./hello 'John Jacob Jingleheimer Schmidt'"
+    result[:output].should eq("Hello John Jacob Jingleheimer Schmidt!")
+  end
+
+  it "errors gracefully with more than one parameter" do
+    result = run "./hello a b c"
+    result[:output].should eq("hello: too many parameters")
+  end
+
+end
+
+def run(command : String)
+  io = MemoryIO.new
+  status = Process.run(command, shell: true, output: io)
+  { output: io.to_s, status: status }
 end
