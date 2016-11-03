d57871c6026aae0188cb3e082004f553bcfc0e69
John W. Long <me@johnwlong.com>
2016-11-03 15:16:50 -0400

    more idiomatic version

--- hello.cr
+++ hello.cr
@@ -1,10 +1,9 @@
-if (ARGV.size > 0)
-  if (ARGV.size < 2)
-    printf("Hello %s!\n", ARGV[0])
-  else
-    abort "hello: too many parameters"
-  end
-else
+case ARGV.size
+when 0
   puts "Hello world!"
+when 1
+  printf "Hello %s!\n", ARGV[0]
+else
+  abort "hello: too many parameters"
 end
-exit(0)
+exit 0
